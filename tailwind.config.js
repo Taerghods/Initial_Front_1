cece5f1615cea2eee7e163d52d99dfcb4e35380d
Taerghods <sanazzt88@gmail.com>
2024-12-10 01:38:24 +0330

    add

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Mulish', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
